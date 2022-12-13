99883a9d09a69602b702c45e05311a0684c84298
hhack <hans.f.hack@googlemail.com>
2022-12-13 07:28:44 +0100

    replace switch with btn

--- src/components/Sidebar/content/SidebarContentList/SwitchEnergy/index.tsx
+++ src/components/Sidebar/content/SidebarContentList/SwitchEnergy/index.tsx
@@ -1,42 +1,34 @@
 import { FC } from 'react'
 import { Switch } from '@headlessui/react'
 
-export interface SwitchEnergy {}
+export interface SwitchEnergyType {}
 
-export const SwitchEnergy: FC<SwitchEnergy> = ({
+export const SwitchEnergy: FC<SwitchEnergyType> = ({
   consumptionType,
   setConsumptionType,
 }) => {
-  function onSwitch(enabled) {
-    if (enabled) {
-      setConsumptionType('entityElectricityUsage')
-    } else {
-      setConsumptionType('entityHeatUsage')
-    }
-  }
-
   return (
-    <div className="top-[76px] py-2 flex sticky bg-secondary">
-      <div className="pr-2 text-sm">Wärme</div>
-      <Switch
-        checked={consumptionType === 'entityElectricityUsage'}
-        onChange={onSwitch}
+    <div className="flex top-[76px] sticky">
+      <button
+        onClick={() => setConsumptionType('entityElectricityUsage')}
         className={`${
           consumptionType === 'entityElectricityUsage'
-            ? 'bg-primary'
-            : 'bg-primary'
-        } relative inline-flex h-6 w-11 items-center rounded-full`}
+            ? 'bg-primary text-secondary'
+            : 'bg-secondary text-primary'
+        } flex-1 rounded`}
       >
-        <span className="sr-only">Enable notifications</span>
-        <span
-          className={`${
-            consumptionType === 'entityElectricityUsage'
-              ? 'translate-x-6'
-              : 'translate-x-1'
-          } inline-block h-4 w-4 transform rounded-full bg-white transition text-center `}
-        />
-      </Switch>
-      <div className="pl-2 text-sm">Strom</div>
+        Strom
+      </button>
+      <button
+        onClick={() => setConsumptionType('entityHeatUsage')}
+        className={`${
+          consumptionType === 'entityHeatUsage'
+            ? 'bg-primary text-secondary'
+            : 'bg-secondary text-primary'
+        } flex-1 rounded`}
+      >
+        Wärme
+      </button>{' '}
     </div>
   )
 }
