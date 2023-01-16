cd5106758517d4e0cd6ef2ac7482d8908b12c645
KlemensM <98896505+KlemensM@users.noreply.github.com>
2023-01-16 21:55:14 +0100

    feat(sidebar) add text

--- src/components/Sidebar/content/SidebarContentInfo/index.tsx
+++ src/components/Sidebar/content/SidebarContentInfo/index.tsx
@@ -30,19 +30,27 @@ export const SidebarContentInfo: FC<SidebarContentInfoType> = ({ }) => {
 
         <Accordion title="Was ist Open Data?">
           <p className="text-sm pb-2">
+            Offene Daten definieren sich dadurch, dass sie in einem offenen und maschinenlesbaren Format vorliegen, unter einer freien Lizenz nutzbar sind, der Zugang diskriminierungsfrei und kostenlos ist und die Daten an einem zentralen Ort dauerhaft auffindbar sind.
+            Open Data ist heute ein wichtiger Bestandteil im Verwaltungshandeln Berlins und schafft nicht nur Transparenz und Offenheit, sondern ermöglicht auch Analysen und Monitorings wie diese, um einen Überblick über wichtige Entwicklungen zu erhalten. Deshalb unterstützt die Open Data Infor­mations­stelle Berliner Behörden bei der Bereit­stellung von Open Data.
+            Mehr offene Daten findest du im Berliner Datenportal.
+          </p>
+        </Accordion>
+
+        <Accordion title="Wie kann ein landeesweites Monitoring entstehen?">
+          <p className="text-sm pb-2">
             WIP
           </p>
         </Accordion>
 
         <Accordion title="Was ist mit anderen Städten?">
           <p className="text-sm pb-2">
-            WIP
+            Der “Berliner Weihnachtsmarkt-Finder” ist ein Open-Source-Projekt und läuft unter einer MIT Lizenz. Dementsprechend kann die Idee, aber auch der Quellcode für die Umsetzung in anderen Städten kostenlos genutzt, angepasst und weiterentwickelt werden. Wenn Du dich dafür interessierst, schau gerne in unserem GitHub-Repository vorbei.
           </p>
         </Accordion>
 
         <Accordion title="Über uns">
           <p className="text-sm pb-2">
-            Der “Berliner EnergieCheckpoint” ist ein Projekt der Open Data Informationsstelle in Zusammenarbeit mit dem CityLAB Berlin.
+            Der “Berliner EnergieCheckpoint” ist ein Projekt der Open Data Informationsstelle Berlin in Zusammenarbeit mit dem CityLAB Berlin.
             Die ODIS wird von der Berliner Senatsverwaltung für Inneres, Digitalisierung und Sport und der Investitionsbank Berlin
             aus den Mitteln des Landes Berlin gefördert und ist ein Projekt der Technologiestiftung Berlin.
             Seit 2018 begleiten wir als ODIS die Stadt auf dem Weg zu einer partizipativen, nachhaltigen und datengetriebenen Gesellschaft
