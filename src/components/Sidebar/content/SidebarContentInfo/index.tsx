9da8d46d13e2cc8244addbd2c0aa062f746223f7
KlemensM <98896505+KlemensM@users.noreply.github.com>
2023-01-16 21:49:09 +0100

    feat(texte) add descriptions and text

--- src/components/Sidebar/content/SidebarContentInfo/index.tsx
+++ src/components/Sidebar/content/SidebarContentInfo/index.tsx
@@ -10,23 +10,48 @@ import { TsbLogo } from '@components/Logos/TsbLogo'
 import { Accordion } from '@components/Accordion'
 import { SenInLogo } from '@components/Logos/SenInLogo'
 
-export interface SidebarContentInfoType {}
+export interface SidebarContentInfoType { }
 
-export const SidebarContentInfo: FC<SidebarContentInfoType> = ({}) => {
+export const SidebarContentInfo: FC<SidebarContentInfoType> = ({ }) => {
   return (
     <>
-      <SidebarHeader text="Über den Finder" />
+      <SidebarHeader text="Über die Karte" />
 
       <SidebarBody>
         <p className="text-sm pt-2 pb-2">
-WIP
+          WIP
         </p>
 
         <Accordion title="Woher kommen die Daten?">
           <p className="text-sm pb-2">
-        WIP 
-        </p>
+            WIP
+          </p>
         </Accordion>
+
+        <Accordion title="Was ist Open Data?">
+          <p className="text-sm pb-2">
+            WIP
+          </p>
+        </Accordion>
+
+        <Accordion title="Was ist mit anderen Städten?">
+          <p className="text-sm pb-2">
+            WIP
+          </p>
+        </Accordion>
+
+        <Accordion title="Über uns">
+          <p className="text-sm pb-2">
+            Der “Berliner EnergieCheckpoint” ist ein Projekt der Open Data Informationsstelle in Zusammenarbeit mit dem CityLAB Berlin.
+            Die ODIS wird von der Berliner Senatsverwaltung für Inneres, Digitalisierung und Sport und der Investitionsbank Berlin
+            aus den Mitteln des Landes Berlin gefördert und ist ein Projekt der Technologiestiftung Berlin.
+            Seit 2018 begleiten wir als ODIS die Stadt auf dem Weg zu einer partizipativen, nachhaltigen und datengetriebenen Gesellschaft
+            mit dem Schwerpunkt auf die Bereitstellung und Nutzung offener Daten.
+            Du hast Feedback oder willst mehr erfahren? Schau dich auf unserer Webseite um oder kontaktiere uns unter odis@ts.berlin.
+          </p>
+        </Accordion>
+
+
 
         <section className="mt-6 flex flex-wrap">
           <div className="flex flex-col mr-6 mb-6">
@@ -40,7 +65,7 @@ WIP
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Open Data Informationsstelle Berlin"
-              // className={}
+            // className={}
             >
               <OdisLogo className={`w-40`} />
             </a>
@@ -53,7 +78,7 @@ WIP
       </SidebarBody>
       <footer className={classNames('mt-8 p-4', 'flex flex-wrap')}>
         <span className="text-xs w-full mb-4">
-          © 2022 Technologiestiftung Berlin
+          © 2023 Technologiestiftung Berlin
         </span>
         <a
           href="https://www.technologiestiftung-berlin.de/de/impressum/"
